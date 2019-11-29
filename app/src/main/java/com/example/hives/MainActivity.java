01e1358b3297b02aafa49c8048564f4192bf89a7
raghadn <alnassar.raghad@gmail.com>
2019-11-29 23:23:24 +0300

    TimeLIne

--- app/src/main/java/com/example/hives/MainActivity.java
+++ app/src/main/java/com/example/hives/MainActivity.java
@@ -11,6 +11,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,13 +43,12 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference allPostdatabaseRef;
     private RecyclerView Post;
     private FirebaseUser Fuser;
-    private ArrayList<String> joinpost=new ArrayList<>();
+    private ArrayList<String> joinpost = new ArrayList<>();
 
     private DatabaseReference ClickPostRef;
-    private DatabaseReference PostRef;
+    private DatabaseReference PostRef,hivesref;
     Dialog myDialog;
     Dialog imageDialog;
-
 
 
     @Override
@@ -65,16 +65,16 @@ public class MainActivity extends AppCompatActivity {
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
 
-        mAuth=FirebaseAuth.getInstance();
-        UsersRef= FirebaseDatabase.getInstance().getReference().child("Users");
-        if (mAuth.getCurrentUser()==null){
+        mAuth = FirebaseAuth.getInstance();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        if (mAuth.getCurrentUser() == null) {
             finish();
             sendUserToStartActivity();
         }
 
 
-        drawerLayout=(DrawerLayout) findViewById(R.id.drawable_layout);
-        bottomnav= (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        bottomnav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomnav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -87,36 +87,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
-        FirebaseUser currentUser=mAuth.getCurrentUser();
-        if (currentUser==null) {
-           sendUserToStartActivity();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            sendUserToStartActivity();
+        } else {
+            if (currentUser.isEmailVerified()) {
+                CheckUserExistence();
+            } else {
+                sendUserToStartActivity();
+            }
         }
-       else {
-           if (currentUser.isEmailVerified()){
-          CheckUserExistence();}
-           else {
-               sendUserToStartActivity();
-           }
-       }
     }
+
     private void sendUserToStartActivity() {
-        Intent loginIntent = new Intent(MainActivity.this,SigninActivity.class);
-        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        Intent loginIntent = new Intent(MainActivity.this, SigninActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
         BrowsePost();
     }
+
     private void CheckUserExistence() {
         //mAuth is Firebase
-        final String current_user_id=mAuth.getCurrentUser().getUid();
-        String loggedinuser=mAuth.getCurrentUser().getUid();
-        OneSignal.sendTag("User_ID",loggedinuser);
+        final String current_user_id = mAuth.getCurrentUser().getUid();
+        String loggedinuser = mAuth.getCurrentUser().getUid();
+        OneSignal.sendTag("User_ID", loggedinuser);
         UsersRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(!dataSnapshot.hasChild(current_user_id))
-                {
+                if (!dataSnapshot.hasChild(current_user_id)) {
                     SendUserToSetUpActivity();
                 }
             }
@@ -128,48 +128,52 @@ public class MainActivity extends AppCompatActivity {
         });
         BrowsePost();
     }
+
     private void SendUserToSetUpActivity() {
-        Intent setupIntent=new Intent(MainActivity.this,SetUpActivity.class);
+        Intent setupIntent = new Intent(MainActivity.this, SetUpActivity.class);
         setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(setupIntent);
         BrowsePost();
     }
+
     private void UserMenuSelector(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.nav_jhives:
-                Intent intentjhives=new Intent(MainActivity.this, joinedHivesActivity.class);
+                Intent intentjhives = new Intent(MainActivity.this, joinedHivesActivity.class);
                 startActivity(intentjhives);
                 break;
 
 
-           case R.id.nav_search:
-                Intent intentsearch=new Intent(MainActivity.this, searchActivity.class);
+            case R.id.nav_search:
+                Intent intentsearch = new Intent(MainActivity.this, searchActivity.class);
                 startActivity(intentsearch);
                 break;
 
             case R.id.nav_profile:
-               Intent intentprofile=new Intent(MainActivity.this, ProfileActivity.class);
+                Intent intentprofile = new Intent(MainActivity.this, ProfileActivity.class);
                 startActivity(intentprofile);
                 break;
             case R.id.nav_notf:
-                Intent intentact=new Intent(MainActivity.this, NotificationActivity.class);
+                Intent intentact = new Intent(MainActivity.this, NotificationActivity.class);
                 startActivity(intentact);
                 break;
         }
 
     }
+
     private void openImage(String image) {
 
         ImageView userimg;
 
         imageDialog.setContentView(R.layout.openimage);
-        userimg =imageDialog.findViewById(R.id.postimage);
+        userimg = imageDialog.findViewById(R.id.postimage);
         Picasso.get().load(image).into(userimg);
         imageDialog.show();
     }
+
     public void openDialog(String UserId) {
 
-        ClickPostRef =  FirebaseDatabase.getInstance().getReference().child("Users").child(UserId);
+        ClickPostRef = FirebaseDatabase.getInstance().getReference().child("Users").child(UserId);
         ClickPostRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -177,25 +181,24 @@ public class MainActivity extends AppCompatActivity {
                 ImageView userimg;
                 myDialog.setContentView(R.layout.custompopup);
 
-                if (dataSnapshot.exists()){
-                String Uname = dataSnapshot.child("username").getValue().toString();
-                String Ubio = dataSnapshot.child("bio").getValue().toString();
-                txtclose = myDialog.findViewById(R.id.username);
-                ubio = myDialog.findViewById(R.id.userbio);
-                txtclose.setText(Uname);
-                ubio.setText(Ubio);
+                if (dataSnapshot.exists()) {
+                    String Uname = dataSnapshot.child("username").getValue().toString();
+                    String Ubio = dataSnapshot.child("bio").getValue().toString();
+                    txtclose = myDialog.findViewById(R.id.username);
+                    ubio = myDialog.findViewById(R.id.userbio);
+                    txtclose.setText(Uname);
+                    ubio.setText(Ubio);
 
 
-                if (dataSnapshot.hasChild("profileimg")) {
-                    String Uimage = dataSnapshot.child("profileimg").getValue().toString();
-                    userimg = myDialog.findViewById(R.id.userimage);
-                    Picasso.get().load(Uimage).into(userimg);
-                }
+                    if (dataSnapshot.hasChild("profileimg")) {
+                        String Uimage = dataSnapshot.child("profileimg").getValue().toString();
+                        userimg = myDialog.findViewById(R.id.userimage);
+                        Picasso.get().load(Uimage).into(userimg);
+                    }
 
-                myDialog.show();
-            }
-            else{
-                    Toast.makeText(MainActivity.this,"لا يوجد مستخدم",Toast.LENGTH_LONG).show();
+                    myDialog.show();
+                } else {
+                    Toast.makeText(MainActivity.this, "لا يوجد مستخدم", Toast.LENGTH_LONG).show();
                 }
             }
 
@@ -208,21 +211,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void BrowsePost() {
-        //Query searchHivesAndInfiQuere = allPostdatabaseRef;
-        mAuth = FirebaseAuth.getInstance();
-        Fuser =mAuth.getCurrentUser();
 
-        allPostdatabaseRef =  FirebaseDatabase.getInstance().getReference().child("Posts");
+    private void BrowsePost() {
+        mAuth = FirebaseAuth.getInstance();
+        Fuser = mAuth.getCurrentUser();
+
+        allPostdatabaseRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+
+
         Post = findViewById(R.id.postview);
         Post.setHasFixedSize(true); //???
         Post.setLayoutManager(new LinearLayoutManager(this));
         DatabaseReference UsersRefhive;
         if (Fuser != null)
-        UsersRefhive = FirebaseDatabase.getInstance().getReference().child("Users").child(Fuser.getUid()).child("Joined Hives");
+            UsersRefhive = FirebaseDatabase.getInstance().getReference().child("Users").child(Fuser.getUid()).child("Joined Hives");
 
 
-        FirebaseRecyclerAdapter<AllPosts, PostViweHolder> FirebaseRecycleAdapter
+        final FirebaseRecyclerAdapter<AllPosts, PostViweHolder> FirebaseRecycleAdapter
                 = new FirebaseRecyclerAdapter<AllPosts, PostViweHolder>
                 (
                         AllPosts.class,
@@ -231,77 +236,109 @@ public class MainActivity extends AppCompatActivity {
                         allPostdatabaseRef.orderByChild("time")
                 ) {
             @Override
-            protected void populateViewHolder(final PostViweHolder postViweHolder,final AllPosts module, final int i) {
+            protected void populateViewHolder(final PostViweHolder postViweHolder, final AllPosts module, final int i) {
 
-                final String PostKey = getRef(i).getKey();
-                /*allPostdatabaseRef.child("hivename").addValueEventListener(new ValueEventListener() {
+
+                final ViewGroup.LayoutParams params = postViweHolder.mViwe.getLayoutParams();
+                postViweHolder.mViwe.setVisibility(View.GONE);
+
+                params.height = 0;
+                params.width = 0;
+                hivesref = FirebaseDatabase.getInstance().getReference().child("HIVES").child(module.getHivename());
+
+                hivesref.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.exists()){*/
 
-                postViweHolder.setDate(module.getDate());
-                postViweHolder.setHiveimage(getApplicationContext(), module.getHiveimage());
-                postViweHolder.setDescription(module.getDescription());
-                postViweHolder.setHivename(module.getHivename());
-                postViweHolder.setPostimage(getApplicationContext(), module.getPostimage());
-                postViweHolder.setTime(module.getTime());
-                postViweHolder.setUsername(module.getUsername());
 
-                postViweHolder.img.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //  Toast.makeText(MainActivity.this,module.getPostimage(),Toast.LENGTH_LONG).show();
-                        String Image = module.getPostimage();
-                        if (Image != null){
-                            openImage(Image);}
+                        if (dataSnapshot.hasChild(Fuser.getUid())) {
+                            postViweHolder.mViwe.setVisibility(View.VISIBLE);
+                            params.width = 1000;
+                            if (module.getPostimage() != null)
+                                params.height = 800;
+                            else
+                                params.height = 300;
+
+
+                            final String PostKey = getRef(i).getKey();
+
+
+                            postViweHolder.setDate(module.getDate());
+                            postViweHolder.setHiveimage(getApplicationContext(), module.getHiveimage());
+                            postViweHolder.setDescription(module.getDescription());
+                            postViweHolder.setHivename(module.getHivename());
+                            postViweHolder.setPostimage(getApplicationContext(), module.getPostimage());
+                            postViweHolder.setTime(module.getTime());
+                            postViweHolder.setUsername(module.getUsername());
+
+
+
+
+
+                            postViweHolder.img.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    //  Toast.makeText(MainActivity.this,module.getPostimage(),Toast.LENGTH_LONG).show();
+                                    String Image = module.getPostimage();
+                                    if (Image != null) {
+                                        openImage(Image);
+                                    }
+                                }
+                            });
+
+                            postViweHolder.name.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    PostRef = FirebaseDatabase.getInstance().getReference().child("Posts").child(PostKey);
+                                    PostRef.addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            //  Toast.makeText(MainActivity.this,"heeeereee",Toast.LENGTH_LONG).show();
+                                            if (dataSnapshot.exists()) {
+                                                String Uid = dataSnapshot.child("uid").getValue().toString();
+                                                //  Toast.makeText(MainActivity.this,Uid,Toast.LENGTH_LONG).show();
+                                                openDialog(Uid);
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+
+                                }
+                            });
+                            postViweHolder.mViwe.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Intent ClickPostIntent = new Intent(MainActivity.this, ViewPost.class);
+                                    ClickPostIntent.putExtra("PostKey", PostKey);
+                                    startActivity(ClickPostIntent);
+                                }
+                            });
+
+
+                        }
+
                     }
-                });
-
-                postViweHolder.name.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        PostRef = FirebaseDatabase.getInstance().getReference().child("Posts").child(PostKey);
-                        PostRef.addValueEventListener(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                              //  Toast.makeText(MainActivity.this,"heeeereee",Toast.LENGTH_LONG).show();
-                                if (dataSnapshot.exists()){
-                                String Uid =  dataSnapshot.child("uid").getValue().toString();
-                              //  Toast.makeText(MainActivity.this,Uid,Toast.LENGTH_LONG).show();
-                                openDialog(Uid);}
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                            }
-                        });
-
-                    }
-                });
-                postViweHolder.mViwe.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent ClickPostIntent = new Intent(MainActivity.this, ViewPost.class);
-                        ClickPostIntent.putExtra("PostKey",PostKey);
-                        startActivity(ClickPostIntent);
-                    }
-                });
-                   /* }}
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
 
                     }
-                });*/
-
-
+                });
 
 
             }
         };
 
+
+
+
         Post.setAdapter(FirebaseRecycleAdapter);
+
+
     }
 
     public static class PostViweHolder extends RecyclerView.ViewHolder {
